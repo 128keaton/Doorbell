131a1200ea93d916d7162245915a9b3737c589f5
Keaton Burleson <keaton.burleson@me.com>
2016-12-31 01:19:41 -0600

    Initial Commit

---
+++ Doorbell/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Doorbell
+//
+//  Created by Keaton Burleson on 12/31/16.
+//  Copyright © 2016 Keaton Burleson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Doorbell/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Doorbell
+//
+//  Created by Keaton Burleson on 12/31/16.
+//  Copyright © 2016 Keaton Burleson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
